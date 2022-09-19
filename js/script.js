e206d7a23967e9c7dcd730e8ddef36e47850f39f
ys3333 <111160792+ys3333@users.noreply.github.com>
2022-09-19 22:59:15 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// lesson 92
+
+
+let d1 = document.createElement("div");
+
+d1.className = "d1";
+d1.innerText = "hello man";
+
+
+// sin.setAttribute("class", "hello")
+
+console.log(d1);
+
+document.body.appendChild(d1);
+
+document.body.createElement("dfgefgvsd");
